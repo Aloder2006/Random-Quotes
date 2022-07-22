dbcdfcee2f0aa61a6098e1752a6fc08b830b3baf
Aloder2006 <82025827+Aloder2006@users.noreply.github.com>
2022-07-22 18:40:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,7 @@ let
 function quoteG() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading...";
-  fetch("http://api.quotable.io/random").then(res => res.json()).then(result => {
-    console.log(result)
+  fetch("http://www.api.quotable.io/random").then(res => res.json()).then(result => {
     quoteContent.innerText = result.content;
     quoteAuthor.innerText = `- ${result.author}`;
     quoteBtn.classList.remove("loading");
